2180b4b5560968f3ae777b358fbe5a85e80b7ad5
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-01-12 20:37:48 +0200

    Install spaCy models

--- backend/llm/gpt_adapter.py
+++ backend/llm/gpt_adapter.py
@@ -12,16 +12,16 @@ client = OpenAI(api_key=api_key)
 
 
 def openai_exchange(messages: list[Message], json_mode: bool = False) -> dict:
-    # logging.info(f"message: {messages[1].content}")
+    logging.info(f"message: {messages[1].content}")
     response_format = "json_object" if json_mode else "text"
     completion = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo-1106",
         response_format={"type": response_format},
         messages=[message.asdict() for message in messages]
     )
     response = completion.choices[0].message.content
     logging.info(f"Received response: {response}")
-    return response
+    return parse_response(response)
 
 
 def parse_response(gpt_response: str) -> dict:
