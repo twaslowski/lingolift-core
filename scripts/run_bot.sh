2e8414b1c9d2a24627d6f9bdd126156ff90997c6
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-01-12 20:47:15 +0200

    Migrate telegram bot to poetry

--- scripts/run_bot.sh
+++ scripts/run_bot.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-if [[ -d telegram_client/venv ]]; then
-  echo "virtual environment exists"
-else
-  python3 -m venv telegram_client/venv
-fi
-
-echo "activating virtual environment"
-source telegram_client/venv/bin/activate
-
-echo "installing dependencies"
-pip install -r telegram_client/requirements.txt
-
 # check that .env exists
 if [ ! -f ./telegram_client/.env ]; then
   echo "Error: .env file does not exist." >&2
@@ -22,4 +10,6 @@ fi
 
 # get current directory path
 export PYTHONPATH=$(git rev-parse --show-toplevel)
-python telegram_client/app.py
+pushd telegram_client > /dev/null
+poetry run python app.py
+popd
