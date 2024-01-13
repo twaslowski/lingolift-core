ad6fa94045a1f3d6fc832e02f4746800515b7883
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-01-13 16:51:21 +0200

    Remove obsolete suggestions wrapper object from model

---
+++ shared/shared/model/model.py
@@ -0,0 +1,1 @@
+# this exists only for boilerplate reasons; if it's not here, poetry won't correctly recognise the package
--- shared/shared/model/response_suggestion.py
+++ shared/shared/model/response_suggestion.py
@@ -4,6 +4,3 @@ from pydantic import BaseModel
 class ResponseSuggestion(BaseModel):
     suggestion: str
     translation: str
-
-class Suggestions(BaseModel):
-    response_suggestions: list[ResponseSuggestion]
