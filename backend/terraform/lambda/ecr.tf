4bab77a7581731baf9185a01cedb87f0224092f1
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-01-22 14:08:23 +0100

    Attach repository policy to allow lambda read access

--- backend/terraform/lambda/ecr.tf
+++ backend/terraform/lambda/ecr.tf
@@ -1,11 +1,31 @@
 module "ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name = "${var.name}-lambda"
+  repository_name                 = "${var.name}-lambda"
   repository_image_tag_mutability = "MUTABLE"
 
   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
   repository_lifecycle_policy       = local.repository_lifecycle_policy
+
+  attach_repository_policy = {
+    Action = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+      "ecr:GetRepositoryPolicy",
+    ]
+    Condition = {
+      StringLike = {
+        "aws:sourceArn" = "arn:aws:lambda:eu-central-1:${data.aws_caller_identity.current.account_id}:function:*"
+      }
+    }
+    Effect    = "Allow"
+    Principal = {
+      Service = "lambda.amazonaws.com"
+    }
+    Sid = "LambdaECRImageRetrievalPolicy"
+  }
 }
 
 locals {
--- backend/terraform/lambda/lambda.tf
+++ backend/terraform/lambda/lambda.tf
@@ -5,6 +5,7 @@ module "lambda_function_container_image" {
   description   = "Provides the translation endpoint for the grammr application"
 
   create_package = false
+  timeout = 8
 
   image_uri    = "${module.ecr.repository_url}:latest"
   package_type = "Image"
