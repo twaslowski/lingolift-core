9f63c493c9fc11afd4405c0df4ff4bc4f248fdc5
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-01-22 16:18:14 +0100

    Remove test dependency on OPENAI_API_KEY environment variable being set

--- backend/test/unit/test_gpt_adapter.py
+++ backend/test/unit/test_gpt_adapter.py
@@ -1,63 +1,61 @@
-import unittest
 from llm import gpt_adapter
 
 
-class TestGptAdapter(unittest.TestCase):
+def test_parse_gpt_response():
+    happy_path = """{
+        "literal_translation": "Hello! How are you?",
+        "words": [
+            {
+                "word": "Moin!",
+                "translation": "Hello!"
+            },
+            {
+                "word": "Wie",
+                "translation": "How"
+            },
+            {
+                "word": "geht",
+                "translation": "goes"
+            },
+            {
+                "word": "es",
+                "translation": "it"
+            },
+            {
+                "word": "dir",
+                "translation": "you"
+            }
+        ]
+    }"""
+    result = gpt_adapter.parse_response(happy_path)
+    assert isinstance(result, dict)
 
-    def test_parse_gpt_response(self):
-        happy_path = """{
-            "literal_translation": "Hello! How are you?",
-            "words": [
-                {
-                    "word": "Moin!",
-                    "translation": "Hello!"
-                },
-                {
-                    "word": "Wie",
-                    "translation": "How"
-                },
-                {
-                    "word": "geht",
-                    "translation": "goes"
-                },
-                {
-                    "word": "es",
-                    "translation": "it"
-                },
-                {
-                    "word": "dir",
-                    "translation": "you"
-                }
-            ]
-        }"""
-        gpt_adapter.parse_response(happy_path)
-        self.assertTrue(True)  # when getting here the previous function has not thrown an exception
 
-    def test_parser_manages_trailing_commas(self):
-        happy_path = """{
-            "literal_translation": "Hello! How are you?",
-            "words": [
-                {
-                    "word": "Moin!",
-                    "translation": "Hello!",
-                },
-                {
-                    "word": "Wie",
-                    "translation": "How",
-                },
-                {
-                    "word": "geht",
-                    "translation": "goes",
-                },
-                {
-                    "word": "es",
-                    "translation": "it",
-                },
-                {
-                    "word": "dir",
-                    "translation": "you",
-                }
-            ]
-        }"""
-        gpt_adapter.parse_response(happy_path)
-        self.assertTrue(True)  # when getting here the previous function has not thrown an exception
+def test_parser_manages_trailing_commas():
+    happy_path = """{
+        "literal_translation": "Hello! How are you?",
+        "words": [
+            {
+                "word": "Moin!",
+                "translation": "Hello!",
+            },
+            {
+                "word": "Wie",
+                "translation": "How",
+            },
+            {
+                "word": "geht",
+                "translation": "goes",
+            },
+            {
+                "word": "es",
+                "translation": "it",
+            },
+            {
+                "word": "dir",
+                "translation": "you",
+            }
+        ]
+    }"""
+    result = gpt_adapter.parse_response(happy_path)
+    assert isinstance(result, dict)
