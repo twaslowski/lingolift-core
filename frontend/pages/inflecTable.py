c48b48aac610a5fde308ef5ae8f46f7aeead620b
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-02-05 17:45:39 +0100

    use actual API gateway endpoint for inflectable
    also implement error handling

--- frontend/pages/inflecTable.py
+++ frontend/pages/inflecTable.py
@@ -1,25 +1,31 @@
 import asyncio
 
 import streamlit as st
+from shared.exception import ApplicationException
+
 from GrammrBot import create_client
-from shared.rendering import Stringifier, MarkupLanguage
 
 
 async def inflectable():
-    client = create_client(use_local=True)
-    stringifier = Stringifier(MarkupLanguage.MARKDOWN)
+    client = create_client()
+    # stringifier = Stringifier(MarkupLanguage.MARKDOWN)
     word = st.text_input("Enter a word")
 
     if word:
-        inflections = await client.fetch_inflections(word)
-        table = markdown_table()
-        for inflection in inflections:
-            case = inflection.morphology.get('Case').upper()
-            number = inflection.morphology.get('Number').upper()
-            replacement_string = f"${case}${number}"
-            print(replacement_string)
-            table = table.replace(replacement_string, inflection.word)
-        st.markdown(table)
+        try:
+            inflections = await client.fetch_inflections(word)
+            table = markdown_table()
+            for inflection in inflections:
+                case = inflection.morphology.get('Case').upper()
+                number = inflection.morphology.get('Number').upper()
+                replacement_string = f"${case}${number}"
+                print(replacement_string)
+                table = table.replace(replacement_string, inflection.word)
+            st.markdown(table)
+        except ApplicationException as e:
+            st.error(e.error_message)
+        except Exception:
+            st.error(f"An unexpected error has occurred")
 
 
 def markdown_table():
