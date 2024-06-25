2480eb89db0599dfe33cf9a826921e68f30f7c60
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-06-25 14:20:28 +0200

    Fix openai api response format parameter

--- lingolift/llm/openai_adapter.py
+++ lingolift/llm/openai_adapter.py
@@ -34,7 +34,7 @@ class OpenAIAdapter(AbstractLLMAdapter):
         # mypy complains about the usage of the create() function, but clearly it works
         completion = self.client.chat.completions.create(  # type: ignore
             model=model_name,
-            response_format=ResponseFormat[response_format],
+            response_format={"type": response_format},
             messages=[message.asdict() for message in messages],
         )
         response = completion.choices[0].message.content
